a4c85cf5518b148e05d00a4542882f789d8a8c80
Alessandro Ferrara <aleferrara1998@gmail.com>
2026-05-16 01:25:03 +0200

    remove envrc

--- hosts/common/nix-settings.nix
+++ hosts/common/nix-settings.nix
@@ -37,11 +37,13 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
+        "https://cache.thalheim.io"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+OfTPnbbGVh6/GpKJuHvx6NZI2Cfo6fTY="
+        "cache.thalheim.io-1:R7msbosLEZKrxk/lKxf9BTjOOH7Ax3H0Qj0/6wiHOgc="
       ];
 
       warn-dirty = false;
