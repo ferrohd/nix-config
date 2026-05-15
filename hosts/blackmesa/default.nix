deb4cc11717cb2599c40a991d0373b0cd34cf226
Alessandro <aleferrara1998@gmail.com>
2026-05-15 22:32:31 +0200

    thunar

--- hosts/blackmesa/default.nix
+++ hosts/blackmesa/default.nix
@@ -31,7 +31,7 @@
   # ── File manager ────────────────────────────────────────────────────────
   programs.thunar = {
     enable = true;
-    plugins = with pkgs; [
+    plugins = with pkgs.xfce; [
       thunar-volman
       thunar-archive-plugin
     ];
--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -44,7 +44,7 @@
   # ── File manager ────────────────────────────────────────────────────────
   programs.thunar = {
     enable = true;
-    plugins = with pkgs; [
+    plugins = with pkgs.xfce; [
       thunar-volman
       thunar-archive-plugin
     ];
